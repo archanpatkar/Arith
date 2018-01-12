b5e294be453c4f552ac9a94a8b8085fb269a0e2e
Archan Patkar <archanpatkar@gmail.com>
2018-01-13 02:32:38 +0530

    Update

--- Main.js
+++ Main.js
@@ -1,1 +1,9 @@
-//Work in Progress
+var ohm = require('ohm-js');
+var arith = require("./Grammer");
+var parser = ohm.grammar(arith);
+
+console.log("Grammer -> " + arith);
+
+var result = parser.match("10 + 10 * 100");
+
+console.log("result -> " + result.succeeded());
