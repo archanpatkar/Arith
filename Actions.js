75f4a39b514f6b52a8a096f5f01ea614059a69fe
Archan Patkar <archanpatkar@gmail.com>
2018-01-20 15:48:28 +0530

    Working 1.0

--- Actions.js
+++ Actions.js
@@ -1,8 +1,13 @@
 var actions = {
-  Exp: function() {},
-  AddExp: function() {},
-  MulExp: function() {},
-  number: function() {}
+  Exp: (e) => e.eval(),
+  AddExp: (e) => e.eval(),
+  MulExp: (e) => e.eval(),
+  AddExp_plus: (n1,_,n2) => n1.eval() + n2.eval(),
+  AddExp_minus: (n1,_,n2) => n1.eval() - n2.eval(),
+  MulExp_times: (n1,_,n2) => n1.eval() * n2.eval(),
+  MulExp_div: (n1,_,n2) => n1.eval() / n2.eval(),
+  MulExp_power: (n1,_,n2) => n1.eval() ** n2.eval(),
+  number: (digits) => parseInt(digits.sourceString)
 }
 
 module.exports = actions
