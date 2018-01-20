c9faaa6c004de6254c49aba277adb63bc714a3d9
Archan Patkar <archanpatkar@gmail.com>
2018-01-20 23:10:30 +0530

    Update

--- Main.js
+++ Main.js
@@ -1,4 +1,6 @@
 var arith = require("./Arith");
 
 
+console.log(arith.eval("10 + 10.798"));
 console.log(arith.eval("10 + 10 + 30 + 2 ** 10"));
+// console.log(arith.eval("[10,20,30,40] ++ [50,60,70,80]"));
