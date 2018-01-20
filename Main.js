dacd28708e46aef641f76aff2e39ad50b3f11c55
Archan Patkar <archanpatkar@gmail.com>
2018-01-21 05:05:58 +0530

    Update

--- Main.js
+++ Main.js
@@ -8,3 +8,5 @@ console.log(arith.eval("[100,200,300,400] -- [10,20,30,40]"));
 console.log(arith.eval("[10,20,30,40] ** [2,2,2,2]"));
 console.log(arith.eval("[10,20,30,40] // [2,2,2,2]"));
 console.log(arith.eval("[1,2,3,4] ^^ [3,3,3,3]"));
+
+console.log(arith.eval("<[1,1,1,1]|[1,1,1,1]> # <[3,3,3,3]|[3,3,3,3]>"));
