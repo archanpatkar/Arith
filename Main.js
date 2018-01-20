ee4ea62b77cf72369889b504dc66d465dff8fdec
Archan Patkar <archanpatkar@gmail.com>
2018-01-21 03:38:58 +0530

    Update

--- Main.js
+++ Main.js
@@ -2,5 +2,9 @@ var arith = require("./Arith");
 
 
 console.log(arith.eval("10 + 10.798"));
-console.log(arith.eval("10 + 10 + 30 + 2 ** 10"));
-// console.log(arith.eval("[10,20,30,40] ++ [50,60,70,80]"));
+console.log(arith.eval("10 + 10 + 30 + 2 ^ 10"));
+console.log(arith.eval("[10,20,30,40] ++ [100,200,300,400]"));
+console.log(arith.eval("[100,200,300,400] -- [10,20,30,40]"));
+console.log(arith.eval("[10,20,30,40] ** [2,2,2,2]"));
+console.log(arith.eval("[10,20,30,40] // [2,2,2,2]"));
+console.log(arith.eval("[1,2,3,4] ^^ [3,3,3,3]"));
