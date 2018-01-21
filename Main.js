e1a13b572f6ac073a82051692b4a27f6ec60e590
Archan Patkar <archanpatkar@gmail.com>
2018-01-21 15:13:41 +0530

    Update

--- Main.js
+++ Main.js
@@ -8,5 +8,13 @@ console.log(arith.eval("[100,200,300,400] -- [10,20,30,40]"));
 console.log(arith.eval("[10,20,30,40] ** [2,2,2,2]"));
 console.log(arith.eval("[10,20,30,40] // [2,2,2,2]"));
 console.log(arith.eval("[1,2,3,4] ^^ [3,3,3,3]"));
+// console.log(arith.eval("[1,2,3,4] . [3,3,3,3]"));
+// console.log(arith.eval("[1,2,3,4] x [3,3,3,3]"));
 
-console.log(arith.eval("<[1,1,1,1]|[1,1,1,1]> # <[3,3,3,3]|[3,3,3,3]>"));
+// console.log(arith.eval("<[1,1,1,1]|[1,1,1,1]> +++ <[3,3,3,3]|[3,3,3,3]>"));
+// console.log(arith.eval("<[1,1,1,1]|[1,1,1,1]> --- <[3,3,3,3]|[3,3,3,3]>"));
+// console.log(arith.eval("<[1,1,1,1]|[1,1,1,1]> *** <[3,3,3,3]|[3,3,3,3]>"));
+// console.log(arith.eval("<[1,1,1,1]|[1,1,1,1]> /// <[3,3,3,3]|[3,3,3,3]>"));
+// console.log(arith.eval("<[1,1,1,1]|[1,1,1,1]> . 5"));
+// console.log(arith.eval("<[1,1,1,1]|[1,1,1,1]> x <[3,3,3,3]|[3,3,3,3]>"));
+// console.log(arith.eval("#<[1,1,1,1]|[1,1,1,1]>"));
