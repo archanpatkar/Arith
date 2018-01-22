645216c109b1a2ea0f791bc3ad6804f8c300c86e
Archan Patkar <archanpatkar@gmail.com>
2018-01-22 17:25:57 +0530

    Update

--- Main.js
+++ Main.js
@@ -2,12 +2,23 @@ var arith = require("./Arith");
 
 
 console.log(arith.eval("10 + 10.798"));
+// console.log(arith.eval("(10 * 10) + 40"))
 console.log(arith.eval("10 + 10 + 30 + 2 ^ 10"));
 console.log(arith.eval("[10,20,30,40] ++ [100,200,300,400]"));
 console.log(arith.eval("[100,200,300,400] -- [10,20,30,40]"));
 console.log(arith.eval("[10,20,30,40] ** [2,2,2,2]"));
 console.log(arith.eval("[10,20,30,40] // [2,2,2,2]"));
 console.log(arith.eval("[1,2,3,4] ^^ [3,3,3,3]"));
+console.log(arith.eval("sin(90)"));
+console.log(arith.eval("cos(90)"));
+console.log(arith.eval("tan(90)"));
+console.log(arith.eval("sec(90)"));
+console.log(arith.eval("cosec(90)"));
+console.log(arith.eval("cot(90)"));
+console.log(arith.eval("e ^ 10"));
+console.log(arith.eval("2 * pi * 5"));
+console.log(arith.eval("_6.74"));//Floor
+console.log(arith.eval("~8.14 * 4"));//Ceil
 // console.log(arith.eval("[1,2,3,4] . [3,3,3,3]"));
 // console.log(arith.eval("[1,2,3,4] x [3,3,3,3]"));
 
